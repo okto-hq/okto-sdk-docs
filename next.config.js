e91fffa9dca2631ba66ebc6b373af35f34c28bdc
Sparsh Agarwal <sparsh.a@coindcx.com>
2024-08-02 16:26:24 +0530

    update nextjs to v14

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
 module.exports = {
   ...withNextra(),
+  output: "export",
   images: {
     unoptimized: true,
-  }
+  },
 };
-
